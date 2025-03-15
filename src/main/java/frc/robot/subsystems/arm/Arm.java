629bb72213e45f5b4189d986660f06c9ac79d018
creative-coconut <iamzainazia@gmail.com>
2025-03-15 19:40:20 -0400

    -

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -7,15 +7,15 @@ package frc.robot.subsystems.arm;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkRelativeEncoder;
+//import com.revrobotics.spark.SparkRelativeEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
   private double[] PID = {4.25, 0, 0.6};
 
   public SparkMax arm = new SparkMax(13, MotorType.kBrushless);
-
   private RelativeEncoder encoder = arm.getAlternateEncoder();
+
   public double armSpeed = 0.01;
   public double[] angles = {0, 35, 35, 90, 180}; //angles in degrees from vertical
 //                          L1 L2  L3  L4  grab
@@ -30,6 +30,6 @@ public class Arm extends SubsystemBase {
 
   public void setPosition(double p){
     currentPosition = p;
-    //set arm position
+    encoder.setPosition(p/72);
   }
 }
